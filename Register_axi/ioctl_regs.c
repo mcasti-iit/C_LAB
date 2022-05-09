6cf80e35a606541c02a35ff6d7d4bb93fc38ce44
mcasti-iit <maurizio.casti@iit.it>
2022-05-09 16:59:34 +0200

    added ioctl_regs.c for Register_axi ioctl operations

---
+++ Register_axi/ioctl_regs.c
@@ -0,0 +1,97 @@
+#include <sys/ioctl.h>                                                                           
+#include <sys/types.h>                                                                           
+#include <sys/stat.h>   
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+#define MAGIC_NUMBER 32
+
+#define DEST_CLKGEN 	"clkgen"   // "ON" / "OFF" / "RESET" 
+#define DEST_LED      "led"      // "ON" / "OFF" 
+
+#define CMD_OFF  		  "off"
+#define CMD_ON   		  "on"
+#define CMD_RESET  	  "reset"
+
+
+#define IOCTL_CLKGEN  0
+#define IOCTL_LED     1
+
+#define OFF    0
+#define ON     1
+#define RESET  2
+
+
+
+int main(int argc, char* argv[])  // " argc" 'e il numero digli argomenti passati, "argv" 'e il vettore che li contiene
+																	// NOTA: in argv[0] e' contenuto il nome del programma (ioctl_regs)
+{
+
+	int fd;								// File descriptor
+	int ioctl_id;
+	int ioctl_arg;
+	int ioctl_tag;
+	int ret;
+	
+	if (argc != 4) {																			// verifia che il numero di argomenti sia 4 
+		fprintf(stderr, "Errato numero di argomenti\n");
+			return -3;
+		}
+	
+	else if (strcmp(argv[2], DEST_CLKGEN) == 0) {					// caso del comando "clkgen" 
+		ioctl_id = IOCTL_CLKGEN;
+		if (strcmp(argv[3], CMD_OFF) == 0)
+			ioctl_arg = OFF;
+		else if (strcmp(argv[3], CMD_ON) == 0)
+			ioctl_arg = ON;	
+		else if (strcmp(argv[3], CMD_RESET) == 0)
+			ioctl_arg = RESET;		
+		else 
+		{
+			fprintf(stderr, "Comando errato: non e' possibie lo stato '%s' per il Clock Generator\n", argv[3]);
+			return -1;
+		}
+	}
+	
+	else if (strcmp(argv[2], DEST_LED) == 0) {						// caso del comando "clkgen" 
+		ioctl_id = IOCTL_LED;
+		if (strcmp(argv[3], CMD_OFF) == 0)
+			ioctl_arg = OFF;
+		else if (strcmp(argv[3], CMD_ON) == 0)
+			ioctl_arg = ON;	
+		else 
+		{
+			fprintf(stderr, "Comando errato: non e' possibie lo stato '%s' per il Led\n", argv[3]);
+			return -1;
+		}
+	}
+	
+	else
+				{
+			fprintf(stderr, "Comando sconosciuto\n");
+			return -1;
+		}
+
+	ioctl_tag = _IOW(MAGIC_NUMBER, ioctl_id, int);				// Combina le informazioni utili ad identificare la ioctl come la sintasi dellea chiamata si aspetta
+
+
+	fd = open(argv[1], O_RDWR);														// Apre il file del driver a caratteri; se fallisce, fornisce il valore -1
+	if (fd = -1)
+		{
+			fprintf(stderr, "Impossibile aprire il file %s \n", argv[1]);
+			return -2;
+		}
+
+	ret = ioctl(fd, ioctl_tag, ioctl_arg);								// chiama la ioctl; se fallisce, fornisce il valore -1
+	if (ret = -1)
+		{
+			fprintf(stderr, "Impossibile eseguire il comando %s \n", argv[2]);
+			return -2;
+		}
+
+
+}

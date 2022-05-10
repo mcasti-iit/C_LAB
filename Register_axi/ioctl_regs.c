a9eb87d8e40bb8c6c02d4d963d703daab7ef9589
mcasti-iit <maurizio.casti@iit.it>
2022-05-10 13:58:09 +0200

    ioctl_regs uses define file

--- Register_axi/ioctl_regs.c
+++ Register_axi/ioctl_regs.c
@@ -6,24 +6,11 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "ioctl_regs_define.h"
 
 
-#define MAGIC_NUMBER 32
-
-#define DEST_CLKGEN 	"clkgen"   // "ON" / "OFF" / "RESET" 
-#define DEST_LED      "led"      // "ON" / "OFF" 
-
-#define CMD_OFF  		  "off"
-#define CMD_ON   		  "on"
-#define CMD_RESET  	  "reset"
 
 
-#define IOCTL_CLKGEN  0
-#define IOCTL_LED     1
-
-#define OFF    0
-#define ON     1
-#define RESET  2
 
 
 

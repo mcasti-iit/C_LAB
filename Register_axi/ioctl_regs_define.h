da851598e93de28da12deb6c9c1e72f26b90bfe3
mcasti-iit <maurizio.casti@iit.it>
2022-05-10 13:55:06 +0200

    Added an IOCTL common #define file

---
+++ Register_axi/ioctl_regs_define.h
@@ -0,0 +1,16 @@
+#define MAGIC_NUMBER 32
+
+#define DEST_CLKGEN 	"clkgen"   // "ON" / "OFF" / "RESET" 
+#define DEST_LED      "led"      // "ON" / "OFF" 
+
+#define CMD_OFF  		  "off"
+#define CMD_ON   		  "on"
+#define CMD_RESET  	  "reset"
+
+
+#define IOCTL_CLKGEN  0
+#define IOCTL_LED     1
+
+#define OFF    0
+#define ON     1
+#define RESET  2
